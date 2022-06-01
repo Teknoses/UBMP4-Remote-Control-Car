db163577febb57b025f82592f86d48a7091efb68
Teknoses <99210035+Teknoses@users.noreply.github.com>
2022-06-01 13:00:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UBMP4 Intro 4 Functions
+ This activity has been deprecated. Please use the activity specific to your
+ UBMP4 version. (eg. https://github.com/mirobotech/UBMP4.1-Intro-4-Functions)
---
+++ UBMP4-Intro-4-Functions.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,14 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=UBMP4-Intro-4-Functions.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/UBMP4-Intro-4-Functions.X.production.hex
+# pro configuration
+CND_ARTIFACT_DIR_pro=dist/pro/production
+CND_ARTIFACT_NAME_pro=UBMP4-Intro-4-Functions.X.production.hex
+CND_ARTIFACT_PATH_pro=dist/pro/production/UBMP4-Intro-4-Functions.X.production.hex
