552e324efddea0f5624568c0dc9ffc241e6b370a
ph661110@wellingtoncdsb.ca <ph661110@wellingtoncdsb.ca>
2022-06-21 17:33:30 +0000

    ALL WORKS, NEED TO ORGANISE IT ALL

--- UBMP4-Intro-4-Functions.X/Intro-4-Functions.c
+++ UBMP4-Intro-4-Functions.X/Intro-4-Functions.c
@@ -9,98 +9,114 @@
 
 // Button constant definitions
 #define EnableA H1OUT
-#define EnableB H6OUT
-#define Aforward H3OUT
 #define Abackward H2OUT
+#define Aforward H3OUT
 #define Bforward H4OUT
 #define Bbackward H5OUT
-#define forward SW3
-#define backward SW2
-#define left SW4
-#define right SW5
+#define EnableB H6OUT
 unsigned char Speed = 255;
-// Program variable definitions
+int command;
 
+enum carstatetype
+{
+    forward,
+    backward,
+    left,
+    right,
+    neutral
+};
+enum carstatetype carstate = neutral;
+// Program variable definitions
 
 int main(void)
 {
     OSC_config();               // Configure internal oscillator for 48 MHz
     UBMP4_config();             // Configure on-board UBMP4 I/O devices
     bluetooth_config();
-	TRISC = 0b00000000;
+	TRISC = 0b00000000;         //Sets H1-H8 Pins as Outputs
     while(1)
 	{ 
-        // if(SW2 == 0 && SW3 == 1 && SW4 == 1 && SW5 == 1) //backwards
-        // {
-        //     EnableA = 1;
-        //     EnableB = 1;
-        //     Abackward = 1;
-        //     Bbackward = 1;
-        // }
-        // else if(SW3 == 0 && SW2 == 1 && SW4 == 1 && SW5 == 1) //forwards
-        // {
-        //     EnableA = 1;
-        //     EnableB = 1;
-        //     Aforward = 1;
-        //     Bforward = 1;
-        // }
-        // else if(SW4 == 0 && SW2 == 1 && SW3 == 1 && SW5 == 1) //Turn right
-        // {
-        //     EnableA = 1;
-        //     EnableB = 1;
-        //     Aforward = 1;
-        //     Bbackward = 1;
-        // }
-        // else if(SW5 == 0 && SW2 == 1 && SW3 == 1 && SW4 == 1) //Turn left
-        // {
-        //     EnableA = 1;
-        //     EnableB = 1;
-        //     Bforward = 1;
-        //     Abackward = 1;
-        // }
-        // else
-        // {
-        //     EnableA = 0;
-        //     EnableB = 0;
-        //     Abackward = 0;
-        //     Bbackward = 0;
-        //     Aforward = 0;
-        //     Bforward = 0;
-        // }
+        if(SW2 == 0)
+        {
+            while(!RCIF);
+        }
+        command = bluetooth_getChar();
+         
+        if(command == 1)
+        {
+            carstate = backward;
+        }
+        else if(command == 2)
+        {
+            carstate = forward;
+        }
+        else if(command == 3)
+        {
+            carstate = left;
+        }
+        else if(command == 4)
+        {
+            carstate = right;
+        }
+        else if(command == 5)
+        {
+            carstate = neutral;
+        }
 
-    //     //Acceleration Idea
-        
-    //         EnableA = 1;
-    //         EnableB = 1;
 
-    //      if(SW5 == 0)       //Decrease Speed
-    //     {
-    //         if(Speed > 0)
-    //         {
-    //             Speed -= 1;
-    //         }
-    //     }
 
-    //     if(SW4 == 0)        // Increase Speed
-    //     {
-    //         if(Speed < 255)
-    //         {
-    //             Speed += 1;
-    //         }
-    //     }
 
-    //  for(unsigned char PWMperiod = 255; PWMperiod != 0; PWMperiod --)
-    //     {
-    //         if(Speed == PWMperiod)
-    //         {
-    //         Aforward = 1;
-    //         Bforward = 1;
-    //         }
-    //         __delay_us(20);
-    //     }
-    //         Aforward = 0;
-    //         Bforward = 0;
-  
+
+
+
+
+
+      if(carstate == backward)
+      {
+        EnableA = 1;
+        EnableB = 1;
+        Abackward = 1;
+        Bbackward = 1;
+        Aforward = 0;
+        Bforward = 0;
+      }
+      else if(carstate == forward)
+      {
+        EnableA = 1;
+        EnableB = 1;
+        Abackward = 0;
+        Bbackward = 0;
+        Aforward = 1;
+        Bforward = 1;
+      }
+        else if(carstate == left)
+      {
+        EnableA = 1;
+          EnableB = 1;
+        Abackward = 1;
+        Bbackward = 0;
+        Aforward = 0;
+        Bforward = 1;
+      }
+         else if(carstate == right)
+      {
+        EnableA = 1;
+        EnableB = 1;
+        Abackward = 0;
+        Bbackward = 1;
+        Aforward = 1;
+        Bforward = 0;
+      }
+      else if(carstate == neutral)
+      {
+        EnableA = 0;
+        EnableB = 0;
+        Abackward = 0;
+        Bbackward = 0;
+        Aforward = 0;
+        Bforward = 0;
+      }
+
 
 
 
