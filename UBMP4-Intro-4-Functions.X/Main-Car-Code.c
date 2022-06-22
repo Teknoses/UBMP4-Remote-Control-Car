20c9fe755b499be98d9fa3c13545dc5520bc4bb8
Teknoses <99210035+Teknoses@users.noreply.github.com>
2022-06-22 12:09:13 -0400

    Everything works
    Only 4 movement directions and 1 speed state.

--- UBMP4-Intro-4-Functions.X/Main-Car-Code.c
+++ UBMP4-Intro-4-Functions.X/Main-Car-Code.c
@@ -14,120 +14,152 @@
 #define Bforward H4OUT
 #define Bbackward H5OUT
 #define EnableB H6OUT
-unsigned char Speed = 255;
-int command;
-
-enum carstatetype
+                                    
+enum carstatetype //sets up different carstates
 {
     forward,
     backward,
     left,
     right,
-    neutral
+    neutral,
 };
-enum carstatetype carstate = neutral;
+enum carstatetype carstate = neutral; //sets carstate as neutral to start
+
+int recievecommand(void);
+void changestate(void); 
+int setstate(void);
+void backwardstate(void);
+void forwardstate(void);
+void leftstate(void);
+void rightstate(void);
+void neutralstate(void);
 // Program variable definitions
 
 int main(void)
 {
     OSC_config();               // Configure internal oscillator for 48 MHz
     UBMP4_config();             // Configure on-board UBMP4 I/O devices
-    bluetooth_config();
-	TRISC = 0b00000000;         //Sets H1-H8 Pins as Outputs
+    bluetooth_config();         // Configures pins on the Microchip to connect to bluetooth
+	TRISC = 0b00000000;         // Sets H1-H8 pins as outputs
     while(1)
 	{ 
-        if(SW2 == 0)
-        {
-            while(!RCIF);
-        }
-        command = bluetooth_getChar();
-         
-        if(command == 1)
-        {
-            carstate = backward;
-        }
-        else if(command == 2)
-        {
-            carstate = forward;
-        }
-        else if(command == 3)
-        {
-            carstate = left;
-        }
-        else if(command == 4)
-        {
-            carstate = right;
-        }
-        else if(command == 5)
-        {
-            carstate = neutral;
-        }
-
-
-
-
-
-
-
-
-
-      if(carstate == backward)
-      {
-        EnableA = 1;
-        EnableB = 1;
-        Abackward = 1;
-        Bbackward = 1;
-        Aforward = 0;
-        Bforward = 0;
-      }
-      else if(carstate == forward)
-      {
-        EnableA = 1;
-        EnableB = 1;
-        Abackward = 0;
-        Bbackward = 0;
-        Aforward = 1;
-        Bforward = 1;
-      }
-        else if(carstate == left)
-      {
-        EnableA = 1;
-          EnableB = 1;
-        Abackward = 1;
-        Bbackward = 0;
-        Aforward = 0;
-        Bforward = 1;
-      }
-         else if(carstate == right)
-      {
-        EnableA = 1;
-        EnableB = 1;
-        Abackward = 0;
-        Bbackward = 1;
-        Aforward = 1;
-        Bforward = 0;
-      }
-      else if(carstate == neutral)
-      {
-        EnableA = 0;
-        EnableB = 0;
-        Abackward = 0;
-        Bbackward = 0;
-        Aforward = 0;
-        Bforward = 0;
-      }
-
-
-
-
-
-
-
-
+        changestate();  
         //RESET
         if(SW1 == 0)
         {
             RESET();
         }
     }
+}
+
+int recievecommand(void)
+{
+    while(!RCIF);
+    int command = bluetooth_getChar();
+    return(command);
+}
+
+
+
+int setstate(void)
+{
+    int command = bluetooth_getChar();
+     if(command == 1)
+    {
+        return(backward);
+    }
+    else if(command == 2)
+    {
+        return(forward);
+    }
+    else if(command == 3)
+    {
+        return(left);
+    }
+    else if(command == 4)
+    {
+        return(right);
+    }
+    else if(command == 5)
+    {
+        return(neutral);
+    }
+}
+
+void changestate(void)
+{
+
+    carstate = setstate();
+
+    if(carstate == backward)
+      {
+        backwardstate();
+      }
+      else if(carstate == forward)
+      {
+        forwardstate();
+      }
+        else if(carstate == left)
+      {
+        leftstate();
+      }
+        else if(carstate == right)
+      {
+        rightstate();
+      }
+      else if(carstate == neutral)
+      {
+        neutralstate();
+      }
+}
+
+void backwardstate(void)
+{
+    
+    EnableA = 1;
+    EnableB = 1;
+    Abackward = 1;
+    Bbackward = 1;
+    Aforward = 0;
+    Bforward = 0;
+}
+
+void forwardstate(void)
+{
+    EnableA = 1;
+    EnableB = 1;
+    Abackward = 0;
+    Bbackward = 0; 
+    Aforward = 1;
+    Bforward = 1;
+}
+
+void leftstate(void)
+{
+    EnableA = 1;
+    EnableB = 1;
+    Abackward = 1;
+    Bbackward = 0;
+    Aforward = 0;
+    Bforward = 1;
+}
+
+void rightstate(void)
+{
+    EnableA = 1;
+    EnableB = 1;
+    Abackward = 0;
+    Bbackward = 1;
+    Aforward = 1;
+    Bforward = 0;
+}
+
+void neutralstate(void)
+{
+    EnableA = 0;
+    EnableB = 0;
+    Abackward = 0;
+    Bbackward = 0;
+    Aforward = 0;
+    Bforward = 0;
 }
